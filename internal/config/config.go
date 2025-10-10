1a431fabcbe965e9176b3907c954bf4637dcb002
sharif ahmad <sharifahmad2061@gmail.com>
2025-10-10 23:18:15 +0700

    load config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,9 @@
 package config
 
 import (
-	"github.com/spf13/viper"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 type Config struct {
@@ -17,5 +18,16 @@ type Config struct {
 }
 
 func Load() *Config {
-	viper.
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config/")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		log.Fatalf("Unable to decode into struct, %v", err)
+	}
+	return &config
 }
