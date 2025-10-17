2500547431117af3da0a8e40bd1a8deed0ff8edd
sharif ahmad <sharifahmad2061@gmail.com>
2025-10-18 00:01:58 +0700

    add SQL queries and schema for trip management along with sqlc config

---
+++ internal/db/queries.sql
@@ -0,0 +1,9 @@
+-- name: GetTripByID :one
+SELECT *
+FROM trip.trip
+WHERE id = $1
+
+-- name: CreateTrip :one
+INSERT INTO trip.trip (name, member_id, start_date, end_date)
+VALUES ($1, $2, $3, $4)
+RETURNING *;
---
+++ internal/db/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE trip.trip (
+   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   name VARCHAR(100) NOT NULL,
+   member_id BIGINT NOT NULL,
+   start_date DATE NOT NULL,
+   end_date DATE NOT NULL
+);
+
+-- Create index on member_id and end_date for efficient queries
+CREATE INDEX idx_trip_member_id_end_date ON trip.trip(member_id, end_date);
+
+-- Add this for efficient deletion queries
+CREATE INDEX idx_trip_end_date ON trip.trip(end_date);
